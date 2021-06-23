812e86b026627c6096bb39d5b12f2a2e3867deee
QiZhang <jason.qizhang.b@gmail.com>
2021-06-23 15:17:35 +0800

    Added common module

--- weshopping-parent/weshopping-eureka/src/main/java/com/qizhang/eureka/EurekaApplication.java
+++ weshopping-parent/weshopping-eureka/src/main/java/com/qizhang/eureka/EurekaApplication.java
@@ -1,4 +1,4 @@
-package com.qizhang.weshopping_eureka;
+package com.qizhang.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class WeshoppingEurekaApplication {
+public class EurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WeshoppingEurekaApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 }
