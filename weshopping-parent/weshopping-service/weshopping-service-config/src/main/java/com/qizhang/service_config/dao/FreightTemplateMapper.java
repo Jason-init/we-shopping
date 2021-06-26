422ca7b1e090d3864ed23289944c6aee3d8fe313
QiZhang <jason.qizhang.b@gmail.com>
2021-06-26 21:43:42 +0800

    config-service micro services done

---
+++ weshopping-parent/weshopping-service/weshopping-service-config/src/main/java/com/qizhang/service_config/dao/FreightTemplateMapper.java
@@ -0,0 +1,8 @@
+package com.qizhang.service_config.dao;
+
+import com.qizhang.service_config_api.pojo.FreightTemplate;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface FreightTemplateMapper extends Mapper<FreightTemplate> {
+
+}
