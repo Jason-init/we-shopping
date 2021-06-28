5831f13964a79386868bc5cd6080a850b5416af8
QiZhang <jason.qizhang.b@gmail.com>
2021-06-28 19:40:53 +0800

    Added request limits for Gateway service

--- weshopping-parent/weshopping-gateway/weshopping-gateway-system/src/main/java/com/qizhang/gateway_system/GatewaySystemApplication.java
+++ weshopping-parent/weshopping-gateway/weshopping-gateway-system/src/main/java/com/qizhang/gateway_system/GatewaySystemApplication.java
@@ -2,12 +2,26 @@ package com.qizhang.gateway_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewaySystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewaySystemApplication.class, args);
+    }
+
+    @Bean
+    public KeyResolver ipKeyResolver() {
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                return Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+            }
+        };
     }
 }
